b9c6dc38d28ac22d36c8828ff1a3f7ac7973ae94
terrence-ou <outerrencedl@gmail.com>
2023-06-09 14:58:21 -0400

    chapter 3 finished

--- JAX_FLAX/chapter_03_vae/utils.py
+++ JAX_FLAX/chapter_03_vae/utils.py
@@ -1,8 +1,23 @@
+''''
+#############################################################
+IMPORTANT:
+
+This python file adapts from David Foster's code provided 
+in his book Generative Deep Learning 2nd Edition:
+https://github.com/davidADSP/Generative_Deep_Learning_2nd_Edition/blob/main/notebooks/03_vae/03_vae_faces/vae_utils.py
+The original code is using Apache-2.0 license; this file adapts 
+the original implementation to the JAX context
+
+#############################################################
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+# Get the vector towards the given feature
 def get_vector_from_label(dataset, embedding_dim, label, encode_fn, state, rng):
+    # Initialize parameters
     current_sum_POS = np.zeros(shape=embedding_dim, dtype=np.float32)
     current_n_POS = 0
     current_mean_POS = np.zeros(shape=embedding_dim, dtype=np.float32)
@@ -20,6 +35,7 @@ def get_vector_from_label(dataset, embedding_dim, label, encode_fn, state, rng):
     total_POS_samples = 5000
     curr_iter = 0
     while current_n_POS < total_POS_samples:
+        # Sampling new POS and NEG images
         batch = next(iter(dataset))
         imgs = batch[0].numpy()
         attributes = batch[1].numpy()
@@ -27,6 +43,7 @@ def get_vector_from_label(dataset, embedding_dim, label, encode_fn, state, rng):
         z_POS = z[attributes==1]
         z_NEG = z[attributes==-1]
         
+        # Updated both mean vector for both POS and NEG samples
         if len(z_POS) > 0:
             current_sum_POS = current_sum_POS + np.sum(z_POS, axis=0)
             current_n_POS += len(z_POS)
@@ -39,6 +56,7 @@ def get_vector_from_label(dataset, embedding_dim, label, encode_fn, state, rng):
             new_mean_NEG = current_sum_NEG / current_n_POS
             movement_NEG = np.linalg.norm(new_mean_NEG - current_mean_NEG)
         
+        # Updated the feature vector
         current_vector = new_mean_POS - new_mean_NEG
         new_dist = np.linalg.norm(current_vector)
         dist_change = new_dist - current_dist
@@ -56,6 +74,7 @@ def get_vector_from_label(dataset, embedding_dim, label, encode_fn, state, rng):
         current_mean_NEG = np.copy(new_mean_NEG)
         current_dist = np.copy(new_dist)
 
+        # When the changing distance is very small, terminate the while loop
         stop_thresh = 8e-2
         if np.sum([movement_POS, movement_NEG]) < stop_thresh:
             current_vector = current_vector / current_dist
@@ -65,3 +84,69 @@ def get_vector_from_label(dataset, embedding_dim, label, encode_fn, state, rng):
         curr_iter += 1
     return current_vector
     
+
+# Plot the feature transitions
+def add_vector_to_images(dataset, feature_vec, encode_fn, decode_fn, state, rng):
+    n_plots = 5
+    factors = np.arange(-4, 5)
+    batch = next(iter(dataset))
+    imgs = batch[0].numpy()
+
+    # Get image embeddings
+    z = encode_fn(state, imgs, rng)
+    
+    fig = plt.figure(figsize=(18, 10))
+    counter = 1
+
+    for i in range(n_plots):
+        img = imgs[i]
+        ax = fig.add_subplot(n_plots, len(factors) + 1, counter)
+        ax.axis('off')
+        ax.imshow(img)
+        counter += 1
+        # Add transition images
+        for factor in factors:
+            new_z_sample = z[i] + feature_vec * factor
+            generated_img = decode_fn(state, np.expand_dims(new_z_sample, axis=0))[0]
+            ax = fig.add_subplot(n_plots, len(factors) + 1, counter)
+            ax.axis('off')
+            ax.imshow(generated_img)
+            counter += 1
+    plt.show()
+
+
+# Morph between two faces
+def morph_faces(dataset, encode_fn, decode_fn, state, rng):
+    factors = np.arange(0.0, 1.0, 0.1)
+
+    sample_faces = next(iter(dataset))[0][:2] # sample two faces
+    imgs = sample_faces.numpy()
+    z = encode_fn(state, imgs, rng)
+    fig = plt.figure(figsize=(18, 8))
+    counter = 1
+
+    face_a = imgs[0]
+    face_b = imgs[1]
+
+    # show original face
+    ax = fig.add_subplot(1, len(factors) + 2, counter)
+    ax.axis('off')
+    ax.imshow(face_a)
+
+    counter += 1
+
+    # plot transitions
+    for factor in factors:
+        factored_z = z[0] * (1 - factor) + z[1] * factor
+        generated_img = decode_fn(state, np.expand_dims(factored_z, axis=0))[0]
+        ax = fig.add_subplot(1, len(factors) + 2, counter)
+        ax.axis('off')
+        ax.imshow(generated_img)
+        counter += 1
+    
+    # show target face
+    ax = fig.add_subplot(1, len(factors) + 2, counter)
+    ax.axis('off')
+    ax.imshow(face_b)
+
+    plt.show()
